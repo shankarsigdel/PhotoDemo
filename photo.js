1c80e91bdd39beed36fa734f7504bc9cccd337eb
LAPTOP-NKGJ4DT9\Admin <sss.sgd26@gmail.com>
2020-09-21 19:32:57 -0400

    update changes

--- photo.js
+++ photo.js
@@ -1,6 +1,32 @@
-function myfunction(){
-    var height=document.getElementById('my_image').height;
-    var width=document.getElementById('my_image').width;
-    document.getElementById('result_height').innerHTML=height;
-    document.getElementById('result_width').innerHTML=width;
+class Photo {
+  constructor(width = 8, height = 10){
+      this.width = width;
+      this.height = height;
   }
+
+  price(){
+      let finalPrice;
+
+      if ((this.width == 8) && (this.height == 10)){
+          finalPrice = 4;
+      } else {
+          if ((this.width == 10) && (this.height == 12)){
+              finalPrice = 6;
+          } else {
+              finalPrice = 10;
+          }
+      }
+
+      return finalPrice;
+  }
+
+  toString(){
+      return `This photo is ${this.width} / ${this.height} and it costs $${this.price()}`;
+  }
+}
+
+let photo1 = new Photo();
+console.log(photo1.toString());
+
+let photo2 = new Photo(12, 15);
+console.log(photo2.toString());
