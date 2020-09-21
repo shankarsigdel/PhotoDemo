22827bc87065f855c05cb139c263435abdb3dbe7
LAPTOP-NKGJ4DT9\Admin <sss.sgd26@gmail.com>
2020-09-21 17:16:05 -0400

    first commit

---
+++ photo.js
@@ -0,0 +1,6 @@
+function myfunction(){
+    var height=document.getElementById('my_image').height;
+    var width=document.getElementById('my_image').width;
+    document.getElementById('result_height').innerHTML=height;
+    document.getElementById('result_width').innerHTML=width;
+  }
